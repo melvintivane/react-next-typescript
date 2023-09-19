f45a417a81c8925b7a2eca59e2f7dce6ed23d8c2
Mello47 <melvinshuster47@gmail.com>
2023-09-19 19:00:21 +0200

    Update PostList component

--- src/components/postList/PostList.tsx
+++ src/components/postList/PostList.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import PostCard from "../postCard/PostCard";
+import { PostProps } from "@/types/types";
 
-const PostList = () => {
-  return (
-    <div>PostList</div>
-  )
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
 }
 
-export default PostList
+const PostList = async () => {
+  const data: PostProps[] = await getData();
+  return (
+    <div className="postList">
+      {/* <PostCard title="Teste" body="description" /> */}
+
+      {data.map((post) => (
+        <PostCard key={post.id} {...post} />
+      ))}
+    </div>
+  );
+};
+
+export default PostList;
